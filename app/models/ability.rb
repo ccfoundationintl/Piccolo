34ff1e581bd1ba4773bf2a4dd3e4dee1380fcd40
Nick Ladd <laddng@gmail.com>
2016-07-25 21:25:31 -0700

    Created user login, signout, authentication, authorization

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+      user ||= User.new # guest user (not logged in)
+      can :manage, User, id: user.id
+      if user.admin?
+        can :manage, :all
+      elsif user.staff_member?
+        can :manage, Charge
+        cannot :read, :admin
+      else
+        can :read, :all
+        cannot :read, :admin
+      end
+  end
+
+end
