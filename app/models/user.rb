de530dc5ef5ec9b451c721eff5a6fd58695446dc
Nick Ladd <laddng@gmail.com>
2016-08-05 11:47:21 -0700

    Added validations to user model to ensure the user has the proper attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,67 @@
 #
 # Table name: users
 #
-#  id             :integer          not null, primary key
-#  first_name     :string
-#  last_name      :string
-#  bio            :text
-#  class_year     :integer
-#  raise_goal     :integer
-#  gender         :string
-#  tshirt         :string
-#  admin          :boolean
-#  contact_name   :string
-#  contact_number :string
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
+#  id                     :integer          not null, primary key
+#  first_name             :string
+#  last_name              :string
+#  bio                    :text
+#  class_year             :integer
+#  raise_goal             :integer
+#  gender                 :string
+#  tshirt                 :string
+#  admin                  :boolean
+#  contact_name           :string
+#  contact_number         :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  avatar_file_name       :string
+#  avatar_content_type    :string
+#  avatar_file_size       :integer
+#  avatar_updated_at      :datetime
 #
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :team
+  has_many :donations, as: :donated
+
+  validates :email, presence: {message: "Your email cannot be blank"}, uniqueness: {message: "This email address is already being used"}
+  validates :password, presence: {message: "Your password cannot be blank"}, length: {minimum: 8, too_short: "Your password must have at least 8 characters"}, on: :create
+  validates :password, length: {minimum: 8, too_short: "Your password must have at least 8 characters"}, on: :update, allow_blank: true
+  validates :first_name, presence: {message: "Your first name cannot be blank"}
+  validates :last_name, presence: {message: "Your last name cannot be blank"}
+  validates :year, presence: {message: "Indicate your year"}
+  validates :goal, presence: {message: "Set your fundraising goal"}
+  validates :gender, presence: {message: "Indicate your gender"}
+  validates :tshirt, presence: {message: "Select your t-shirt size"}
+
+  has_attached_file :avatar, styles: { large: "800x800>",  medium: "400x400>", thumb: "100x100>" }, default_url: "/app_icon.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validate :password_complexity, on: :create
+
+  def password_complexity
+    if password.present? and not password.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[_\W])/)
+      errors.add :password, "Your password must include at least one lowercase letter, one uppercase letter, one digit, and one symbol."
+    end
+  end
+
+  def self.search(query)
+    query = query.split.map(&:capitalize).join(' ')
+    where("first_name LIKE ? OR last_name LIKE ? OR CONCAT(first_name,' ',last_name) LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%")
+  end
+
+  accepts_nested_attributes_for :team, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,19 +2,33 @@
 #
 # Table name: users
 #
-#  id             :integer          not null, primary key
-#  first_name     :string
-#  last_name      :string
-#  bio            :text
-#  class_year     :integer
-#  raise_goal     :integer
-#  gender         :string
-#  tshirt         :string
-#  admin          :boolean
-#  contact_name   :string
-#  contact_number :string
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
+#  id                     :integer          not null, primary key
+#  first_name             :string
+#  last_name              :string
+#  bio                    :text
+#  class_year             :integer
+#  raise_goal             :integer
+#  gender                 :string
+#  tshirt                 :string
+#  admin                  :boolean
+#  contact_name           :string
+#  contact_number         :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  avatar_file_name       :string
+#  avatar_content_type    :string
+#  avatar_file_size       :integer
+#  avatar_updated_at      :datetime
 #
 
 require 'test_helper'
