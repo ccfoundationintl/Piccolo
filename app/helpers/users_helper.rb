c40f5777b6a4198e1397e9fff65b752f61ece6ac
Nick Ladd <laddng@gmail.com>
2016-08-09 10:44:34 -0700

    Created two helper methods for finding user's first name as well as amount raised

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def name(user)
+    "#{user.first_name} #{user.last_name}"
+  end
+
+  def amount_raised(user)
+    user.donations.where(is_donation: true).sum(:amount)
+  end
+
 end
