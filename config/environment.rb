5bceb201b146f877ecc05dfe65f6895966e4a732
Nick Ladd <laddng@gmail.com>
2016-08-10 23:21:48 -0400

    Updates to configuration variables

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,14 @@ Rails.application.initialize!
 
 Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
 Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+
+# for sending emails through SendGrid
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'xxx.herokuapp.com',
+  :enable_starttls_auto => true
+}
