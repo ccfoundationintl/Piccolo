7ef8e4d3eff3fd0edb924c05afdef9f6a55f6879
Nick Ladd <laddng@gmail.com>
2016-08-23 21:05:56 -0400

    Added default email url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  config.action_mailer.default_url_options = { :host => APP_CONFIG['url_to_site']}
+
 end
