3cd826186e8cdaff09a77072383833dc5abc4392
Graham Turner <turnertgraham@gmail.com>
2018-08-04 22:59:11 -0400

    Display progress bar on the home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,9 @@ class WelcomeController < ApplicationController
     @top_10_users = User.joins(:donations).where('donations.is_donation=true').select('users.id, first_name, last_name, sum(donations.amount) as total_raised').group('users.id').order('total_raised desc').limit(10)
     @sponsors = Sponsor.all
     @donation = Donation.new
+
+    @amount_raised = Team.all.sum(:amount_raised)
+    @goal = User.all.sum(:raise_goal)
+    @amount_raised_goal =  (@amount_raised.to_f / @goal.to_f) * 100
   end
 end
